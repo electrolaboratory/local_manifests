628db80206df03e8c7dd89f03a3710a160aa880a
Meisie Widuri <dotkit@electrowizard.me>
2025-03-04 19:23:22 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ rm -rf .repo/local_manifests/
 
 repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.1 --git-lfs
 
-git clone --depth=1 https://github.com/electrolaboratory/local_manifests -b main .repo/local_manifests
+git clone --depth=1 https://github.com/electrolaboratory/local_manifests -b axion .repo/local_manifests
 
 if [ -f /usr/bin/resync ]; then
    /usr/bin/resync
@@ -15,7 +15,6 @@ else
 fi
 
 . build/envsetup.sh
-breakfast X00TD userdebug
-gk -s
+lunch lineage_X00TD-ap4a-userdebug
 make installclean
 mka bacon

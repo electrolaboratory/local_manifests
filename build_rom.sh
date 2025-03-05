93e16b2ceef0ecb794044fce01646f60cb4e17e3
Meisie Widuri <dotkit@electrowizard.me>
2025-03-05 20:51:53 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-#set -e
-
-cyan='\033[0;36m'
-blue='\033[0;34m'
-nocol='\033[0m'
-
-echo -e "$cyan***********************************************"
-echo -e "            BUILDING ROM from Devspaces CLI         "
-echo -e "**********************************************$nocol"
-
-echo -e "$blue***********************************************"
-echo -e "         FOSS the TOP free server! Always TOP!       "
-echo -e "**********************************************$nocol"
+export TZ=Asia/Jakarta
 
 rm -rf .repo/local_manifests/
 repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 git clone --depth=1 https://github.com/electrolaboratory/local_manifests -b main .repo/local_manifests
-/opt/crave/resync.sh
+
+if [ -f /usr/bin/resync ]; then
+   /usr/bin/resync
+else
+   /opt/crave/resync.sh
+fi
+
 . build/envsetup.sh
 lunch lineage_X00TD-ap4a-userdebug
 export TZ=Asia/Jakarta
